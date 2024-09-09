6a6ac0eea12e18a84c0ce146a3e2a5e9e01ed141
Asad Mirza <asadbmirza97@gmail.com>
2024-09-08 22:49:37 -0400

    created navbar using react bootstrap

--- vite-frontend/src/App.jsx
+++ vite-frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header';
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className="projectHeader">
+      <Header />
+    </div>
   )
 }
 
