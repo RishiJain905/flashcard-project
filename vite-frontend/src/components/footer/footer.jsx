9650d0aba353be2913dfc9446d47db2c32eb561a
Asad Mirza <asadbmirza97@gmail.com>
2024-09-13 00:13:01 -0400

    fixed footer overflow issue

--- vite-frontend/src/components/footer/footer.jsx
+++ vite-frontend/src/components/footer/footer.jsx
@@ -4,26 +4,28 @@ import Github from "/src/assets/github.webp";
 
 export default function Footer({}) {
   return (
-    <Row className="footer">
-      <Col xs={12}>Creators</Col>
-      <Col md={4}>
-        <p>Asad Mirza</p>
-        <a href="http://github.com/asadbmirza">
-          <img className="githubLogo" src={Github} alt="githubLogo" />
-        </a>
-      </Col>
-      <Col md={4}>
-        <p>Rishi Jain</p>
-        <a href="http://github.com/RishiJain905">
-          <img className="githubLogo" src={Github} alt="githubLogo" />
-        </a>
-      </Col>
-      <Col md={4}>
-        <p>Moiz Mohammad</p>
-        <a href="http://github.com/moizmohammad">
-          <img className="githubLogo" src={Github} alt="githubLogo" />
-        </a>
-      </Col>
-    </Row>
+    <div className="footer">
+      <Row>
+        <Col xs={12}>Creators</Col>
+        <Col md={4}>
+          <p>Asad Mirza</p>
+          <a href="http://github.com/asadbmirza">
+            <img className="githubLogo" src={Github} alt="githubLogo" />
+          </a>
+        </Col>
+        <Col md={4}>
+          <p>Rishi Jain</p>
+          <a href="http://github.com/RishiJain905">
+            <img className="githubLogo" src={Github} alt="githubLogo" />
+          </a>
+        </Col>
+        <Col md={4}>
+          <p>Moiz Mohammad</p>
+          <a href="http://github.com/moizmohammad">
+            <img className="githubLogo" src={Github} alt="githubLogo" />
+          </a>
+        </Col>
+      </Row>
+    </div>
   );
 }
