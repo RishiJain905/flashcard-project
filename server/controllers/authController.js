f50cea6201d47a22254f6db4c5c888dc5555068d
Moiz <mohammedmoiz176@gmail.com>
2024-09-16 16:36:08 -0400

    Finished register user function

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,6 +5,44 @@ import bodyParser from "body-parser";
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+function checkPassword(password)
+{
+    const re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,12}$/;
+    return re.test(str);
+}
+
+async function checkEmail(email){
+    const result = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+    if(result.rows.length > 0) return false;
+    return true;
+}
+
 export const registerUser = async(req, res) => {
     const {fname, lname, email, password} = req.body;
-}
+    try{
+        if(!checkPassword(password)) return res.status(400).json({message: "Invalid Password"});
+        if(!checkEmail(email)) return res.status(400).json({message: "Email already in use"});
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await pool.query("INSERT INTO USERS(fname, lname, email, password) VALUES($1, $2, $3, $4) RETURNING*"
+            ,[fname, lname, email, password]
+        );
+
+        return res.status(201).json({message: "User Registered Successfully", user: newUser.rows[0]});
+
+    }catch(err){
+        console.error(err.message);
+        return res.status(500).json({message: "Server Error"});
+    }
+};
+
+export const userLogin = async(req, res) => {
+    const {email, password} = req.body;
+    try{
+        
+    }catch(err){
+        console.error(err.message);
+        return res.status(500).json({message: "Server Error"});
+    }
+};
