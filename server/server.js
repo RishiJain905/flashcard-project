9513399a87813055560ac16cdf772f7597585403
Moiz <mohammedmoiz176@gmail.com>
2024-09-16 22:48:15 -0400

    Added cors for connection with frontend

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import authRoutes from "./routes/authRoutes.js";
+import cors from "cors";
 
 const app = express();
 const port = 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 app.use("/api/auth", authRoutes);
 
 app.listen(port, ()=>{
